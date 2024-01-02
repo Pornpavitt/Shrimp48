173c405f09869fc5f67d1a50a5c8436157bc1da8
Fury <88502618+Pornpavitt@users.noreply.github.com>
2024-01-02 16:22:40 +0700

    result1
    d

--- app_shrimp_type/views.py
+++ app_shrimp_type/views.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from django.shortcuts import render
 from django.http import HttpResponse
 
@@ -8,4 +9,7 @@ def shrimptypes(request):
     return render(request, 'app_shrimp_type/shrimptypes.html')
 
 def shrimptype(request, shrimp_id):
-    return render(request, 'app_shrimp_type/shrimptypes.html', context={"shrimp_id": shrimp_id})
+   d = {'col1': [1, 2], 'col2': [3, 4]}
+   df = pd.DataFrame(data=d)
+   return render(request, 'app_shrimp_type/shrimptypes.html', context={"shrimp_id": shrimp_id,"df":df})
+
