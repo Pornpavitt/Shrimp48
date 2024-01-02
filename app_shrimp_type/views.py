939bb64c3cd87510c0fe570d5dc10a8d7897053d
Pornpavit <pornpavitthayommai@gmail.com>
2024-01-02 15:52:55 +0700

    views.py

--- app_shrimp_type/views.py
+++ app_shrimp_type/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+
 # Create your views here.
 
 def shrimptypes(request):
     return render(request, 'app_shrimp_type/shrimptypes.html')
 
 def shrimptype(request, shrimp_id):
-    return HttpResponse('กุ้งตัวนี้ ID = '+ str(shrimp_id))
+    return render(request, 'app_shrimp_type/shrimptypes.html', context={"shrimp_id": shrimp_id})
